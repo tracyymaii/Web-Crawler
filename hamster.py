a3d80b840fd853e53ee710d562f6dbfa9371f6d0
tracyymaii <tracyttmai@gmail.com>
2025-02-21 00:34:25 -0800

    made barebones graph

--- hamster.py
+++ hamster.py
@@ -1,15 +1,10 @@
 import networkx as nx # to make the graph
-import pandas as pd # to handle tabular data
+# import matplotlib.pyplot as plt
+# import pandas as pd # to handle tabular data
 
-# Nodes: 875713 Edges: 5105039
-
-G = nx.DiGraph() # directed graph
-
-# Read the file while ignoring comments
-df_edges = pd.read_csv("web-Google.txt", sep = " ", comment = "#", header = None, names = ["FromNodeId", "ToNodeId"])
-
-edges_list = list(df_edges.itertuples(index = False, name = None)) # index = false, excludes row index, name = none, regular tuples > named tuples
-
-G.add_edges_from(edges_list)
+G = nx.read_edgelist('web-Google.txt', create_using=nx.DiGraph(), nodetype=int)
 
 print("Graph successfully created with", G.number_of_nodes(), "nodes and", G.number_of_edges(), "edges.")
+
+# nx.draw(G)
+# plt.show()
