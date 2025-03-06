c9a0cec34679cabee6ea6869aeffdd283c18de48
MinnieMwooh <caog@spu.edu>
2025-03-05 23:53:10 -0800

    add comments

--- mango.py
+++ mango.py
@@ -1,12 +1,12 @@
 import matplotlib
-matplotlib.use('TkAgg')
+matplotlib.use('TkAgg') # allow pop-up GUI
 
 import networkx as nx # to make the graph
 import pandas as pd # handle data
 import matplotlib.pyplot as plt # graph visual 
-import argparse
+import argparse # get arguments
 
-# get closeness centrality list
+# return closeness centrality list
 def closeness_centrality(G):
     n = len(G)
     centrality = {}
@@ -21,25 +21,32 @@ def closeness_centrality(G):
 
     return centrality
 
+# save data to file
 def save_to_csv(data, filename, columns = []):
     if columns:
         df = pd.DataFrame(data.items(), columns = columns)
     else:
         df = pd.DataFrame(data.items())
-    df.to_csv(filename, index=False)
+    df.to_csv(filename, mode = "w", index = False)
 
+# draw graph G in spring layout, with most central node having a different color
 def draw_graph(G):
+    # find most central node in G
     closeness_centrality = nx.closeness_centrality(G)
     most_central_node = max(closeness_centrality, key=closeness_centrality.get)
 
+    # most central node is green and other nodes are pink
     node_colors = ["yellowgreen" if node == most_central_node else "pink" for node in G]
 
+    # draw, save, and show graph
     plt.figure(figsize = (9,8))
-    nx.draw_spring(G, arrows = True, with_labels = True, node_color = node_colors, node_size = 1500, font_size = 10)
-    plt.savefig("graph.png")
-    plt.show()
+    nx.draw_spring(G, arrows = True, with_labels = True, node_color = node_colors, node_size = 1500, font_size = 10)    # draw graph
+    plt.savefig("graph.png")    # save graph as png
+    plt.show()  # show pop-up GUI
 
+# main function
 def main():
+    # process argument from command line
     parser = argparse.ArgumentParser(description = "This script read edge list from txt file and ...")
     
     parser.add_argument("filename", type = str, help = "Input file name (e.g., web-Google.txt)")
@@ -48,8 +55,13 @@ def main():
 
     args = parser.parse_args()
 
+    # create initial graph
     G = nx.read_edgelist(args.filename, create_using=nx.DiGraph(), nodetype=int)
 
+    # no flag scenario: 
+    # - save all pair shortest path and closeness centrality list for node start from 123 with depth of 5 in separate files
+    # - output most central node
+    # - output longest path
     if not args.g and not args.p:
         print("processing", args.filename)
 
@@ -64,21 +76,22 @@ def main():
         save_to_csv(closeness_centrality, "closeness-123-5.csv", ["node", "value"])
         print("created: closeness-123-5.csv")
 
-        # Find the node with the highest centrality
+        # find the node with the highest centrality
         most_central_node = max(closeness_centrality, key=closeness_centrality.get)
         print("most central node:", most_central_node)
 
+    # with -g: show sub graph starting at node with depth provided in arguments
     if args.g:
         print("drawing graph")
         subG = nx.ego_graph(G, args.g[0], radius=args.g[1], center=True, undirected=False)
         draw_graph(subG)
         print("done drawing")
-    elif args.p:
+    # with -p: output shortest path length from two given nodes
+    if args.p:
         print("processing shortest path from", args.p[0], "to", args.p[1])
         shortest_path = nx.shortest_path_length(G, source=args.p[0], target=args.p[1])
-        print("Shortest Path Length from", args.p[0], "to", args.p[1], ":", shortest_path)
+        print("shortest path length from", args.p[0], "to", args.p[1], ":", shortest_path)
 
-    print("done!")
 
 if __name__ == "__main__":
     main()
